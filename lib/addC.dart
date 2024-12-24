08db621d06849d10d68440266cd471cfd37b8c1a
abhisheksabale <abhisheksable785@gmail.com>
2024-12-24 14:17:56 +0530

    new changed

--- lib/addC.dart
+++ lib/addC.dart
@@ -16,7 +16,7 @@ class AddCategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add Shivam')),
+      appBar: AppBar(title: Text('Grocery app')),
       
       body: Padding(
         padding: const EdgeInsets.all(16.0),
